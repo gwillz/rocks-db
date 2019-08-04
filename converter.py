91bc519998cd93025a838e009e0fdf20e44c56f5
gwillz <gwillz@internode.on.net>
2019-08-04 13:43:35 +0930

    Changed to a 'replacer' algorithm.

--- converter.py
+++ converter.py
@@ -5,30 +5,37 @@ import sys
 def main():
     mapped = load("description-database.txt")
     
+    print("Enter geology descriptions: ")
+    print("Comma separated, type 'q' to quit.")
+    
     # Forever, until we break.
     while True:
-        print("Enter geology descriptions: ")
-        print("Comma separated, type 'q' to quit.")
-        names = input(">> ").strip()
+        phrases = input(">> ").strip()
         
         # Break here, if we want to quit.
-        if not names or names.lower().startswith('q'):
+        if not phrases or phrases.lower().startswith('q'):
             break
         
-        names = [name.strip() for name in names.split(",")]
+        # Split and clean.
+        phrases = [n.strip() for n in phrases.split(",")]
+        shorter = []
         
         print("")
+        print("The acronyms are:")
         
-        for name in names:
-            print(name + ":")
-            
-            if name in mapped:
-                for abbr in mapped[name]:
-                    print("->", abbr)
-            else:
-                print(":: no results.")
+        for phrase in phrases:
+            shorter.append(replace(mapped, phrase))
         
+        print(", ".join(shorter))
         print("")
+
+
+def replace(mapped, phrase):
+    for fragment, abbr in mapped.items():
+        if fragment not in phrase: continue
+        phrase = phrase.replace(fragment, abbr)
+    
+    return phrase.replace(" to ", "-")
 
 
 def load(path):
@@ -37,17 +44,32 @@ def load(path):
     
     # Open file.
     with open(path, "r") as db:
+        # Load the file and clean out line breaks.
+        contents = db.read().replace(r"[\r\n]", "")
+        
         # Load each mapping.
-        for token in db.read().split(","):
+        for token in contents.split(","):
             # Split and clean up strings.
             parts = [part.strip() for part in token.split("=")]
             
+            # The first part is the abbreviation.
+            # All following parts are alternate descriptions.
+            abbreviation, *fragments = parts
+            
             # Store.
-            name, *descriptions = parts
-            
-            for desc in descriptions:
-                mapped[desc] = name
-            
+            for fragment in fragments:
+                mapped[fragment] = abbreviation
+    
+    # Fix up duplicates.
+    extra = {}
+    for phrase, abbr in mapped.items():
+        if phrase.count == 0: continue
+        
+        phrase = replace(mapped, phrase)
+        extra[phrase] = abbr
+    
+    mapped.update(extra)
+    
     return mapped
 
 
